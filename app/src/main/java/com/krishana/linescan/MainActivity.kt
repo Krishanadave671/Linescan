6111321f1e99e6dcba08efe30fc996c6c47c3d94
Krishana Dave <krishnadave671@gmail.com>
2021-12-22 19:22:30 +0530

    Linescan app started by abhinay, piush , yash and krishana

---
+++ app/src/main/java/com/krishana/linescan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.krishana.linescan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
